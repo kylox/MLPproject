d1946ae87e4b19aef0ebf747646ee6f0b21eb938
unknown <maxime.gaudron@hotmail.fr>
2013-03-28 14:48:29 +0100

    -a

---
+++ MLPproject/MLPproject/Map.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace MLPproject
+{
+    class Map
+    {
+        int[,] plateau_tile = new int[32,32];
+        Type_unite[,] plateau_unité = new Type_unite[32, 32];
+
+        public Map()
+        {
+
+
+
+        }
+
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            for (int i = 0; i < plateau_tile.GetLength(1); i++)
+			{
+			 for (int j = 0; j < plateau_tile.GetLength(0); j++)
+			{
+                 switch(plateau_tile[i,j])
+                 {
+                     case 0: 
+
+                         break;
+                     case 1:
+                         break;
+
+
+                     case 2:
+
+                         break;
+
+
+
+
+
+                 }
+			 
+			}
+			}
+
+
+
+
+
+
+        }
+
+    }
+}
---
+++ MLPproject/MLPproject/Unite.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace MLPproject
+{
+    enum Type_unite
+    {
+        legere,
+        rapide,
+        lourde
+    };
+
+    class Unite
+    {
+        int Attaque;
+        int Defense;
+        int Joueur;
+       
+        Type_unite Type;
+        Vector2 Position;
+        Texture2D Sprite;
+
+        public Unite(int joueur, int attaque, int defense, Texture2D sprite, Vector2 position, Type_unite type)
+        {
+            this.Attaque = attaque;
+            this.Defense = defense;
+            this.Joueur = joueur;
+            this.Position = position;
+            this.Sprite = sprite;
+            this.Type = type;
+        }
+
+        public void Deplacement(int x, int y)
+        {
+
+            Position.X = x;
+            Position.Y = y;
+        }
+
+        public bool Is_inbounds()//destination in bounds (a la souris)
+        {
+            switch (Type)
+            {
+                case Type_unite.legere:
+                    //a completer
+                    break;
+
+                case Type_unite.rapide:
+                    //a completer
+                    break;
+                case Type_unite.lourde:
+                    //a completer
+                    break;
+
+
+
+            }
+
+
+            return false;
+        }
+
+
+        public void Update(GameTime gametime)
+        {
+
+
+
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            spritebatch.Begin();
+            spritebatch.Draw(Sprite, Position, Color.White);
+            spritebatch.End();
+
+        }
+
+    }
+}
