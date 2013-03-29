7ed0f23e6ed15328074b9536aacc5b3bd55b37e5
Grimpow <grimpow@gpw.fr>
2013-03-29 08:26:49 +0100

    ...

---
+++ MLPproject/MLPproject/Map.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace MLPproject
+{
+    class Map
+    {
+        Tile[,] plateau_tile = new Tile[32, 32];
+        List<Unite> listUnits = new List<Unite>();
+        List<Ville> listVilles = new List<Ville>();
+        List<Texture2D> listTileTextures = new List<Texture2D>();
+        public Map()
+        {
+            // Generation des tiles avec des textures aleatoires
+            Random r = new Random();
+            for (int i = 0, d = plateau_tile.GetLength(0); i < d; i++)
+                for (int j = 0, c = plateau_tile.GetLength(1); j < c; j++)
+                    plateau_tile[i, j] = new Tile((Type_tile)r.Next(3));
+
+        }
+
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            // Affichage de toutes les tiles
+            for (int i = 0, c = plateau_tile.GetLength(1); i < c; i++)
+                for (int j = 0, d = plateau_tile.GetLength(0); j < d; j++)
+                    plateau_tile[j, i].Draw(spritebatch);
+
+
+
+
+
+
+        }
+
+    }
+}
---
+++ MLPproject/MLPproject/Unite.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace MLPproject
+{
+    enum Type_unite
+    {
+        legere,
+        rapide,
+        lourde
+    };
+
+    class Unite
+    {
+        int Attaque;
+        int Defense;
+        int Joueur;
+        int vitesse;
+        Type_unite Type;
+        Vector2 Position;
+        Texture2D Sprite;
+
+        public Unite(int joueur, Vector2 position, Type_unite type)
+        {
+            switch (Type)
+            {
+                case Type_unite.legere:
+                    Attaque = 2;
+                    Defense = 2;
+                    vitesse = 2;
+                    Sprite = null; // mettre la texure 
+                    break;
+                case Type_unite.rapide:
+                    Attaque = 1;
+                    Defense = 1;
+                    vitesse = 4;
+                    Sprite = null;//mettre texutre
+                    break;
+                case Type_unite.lourde:
+                    Attaque = 3;
+                    Defense = 3;
+                    vitesse = 1;
+                    Sprite = null; // mettre texture
+                    break;
+            }
+
+
+
+            this.Joueur = joueur;
+            this.Position = position;
+
+            this.Type = type;
+
+        }
+
+        public void Deplacement(int x, int y)
+        {
+
+            Position.X = x;
+            Position.Y = y;
+        }
+
+        public bool Is_inbounds(Vector2 destination)//destination in bounds (a la souris)
+        {
+            switch (Type)
+            {
+                case Type_unite.legere:
+                    if (Math.Abs(destination.X - Position.X) < 2 || Math.Abs(destination.X - Position.X) < 2)
+                        return false;
+                    break;
+
+                case Type_unite.rapide:
+                    if (Math.Abs(destination.X - Position.X) < 4 || Math.Abs(destination.X - Position.X) < 4)
+                        return false;
+                    break;
+                case Type_unite.lourde:
+                    if (Math.Abs(destination.X - Position.X) < 1 || Math.Abs(destination.X - Position.X) < 1)
+                        return false;
+                    break;
+
+            }
+
+            return true;
+        }
+
+
+        public void Update(GameTime gametime)
+        {
+
+
+
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            spritebatch.Begin();
+            spritebatch.Draw(Sprite, Position, Color.White);
+            spritebatch.End();
+
+        }
+
+    }
+}
